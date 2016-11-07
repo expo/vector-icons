241874e43736bffeca8ec0501151c9ae42cd49ff
Brent Vatne <brentvatne@gmail.com>
2016-11-07 22:52:10 +0000

    Update for new vector-icon support
    fbshipit-source-id: 808c8ac

--- SimpleLineIcons.js
+++ SimpleLineIcons.js
@@ -1,4 +1,4 @@
-import glyphMap from 'react-native-vector-icons/glyphmap/SimpleLineIcons.json';
+import glyphMap from 'react-native-vector-icons/glyphmaps/SimpleLineIcons.json';
 import createIconSet from './createIconSet';
 
-export default createIconSet(glyphMap, 'simple-line-icons', require('./fonts/SimpleLineIcons.ttf');
+export default createIconSet(glyphMap, 'simple-line-icons', require('./fonts/SimpleLineIcons.ttf'));
