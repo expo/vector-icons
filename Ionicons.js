ee022752a39e15d52e215b170daa37e8f49ae70b
Brent Vatne <brentvatne@gmail.com>
2016-11-08 06:11:50 +0000

    Bump version, fix typo
    fbshipit-source-id: 778c19fd

--- Ionicons.js
+++ Ionicons.js
@@ -1,4 +1,4 @@
-import glyphMap from 'react-native-vector-icons/glyphmaps/Foundation.json';
+import glyphMap from 'react-native-vector-icons/glyphmaps/Ionicons.json';
 import createIconSet from './createIconSet';
 
 export default createIconSet(glyphMap, 'ionicons', require('./fonts/Ionicons.ttf'));
