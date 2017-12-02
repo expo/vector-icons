c22a7981c87806e250979be44b148bcd1c011df4
Brent Vatne <brentvatne@gmail.com>
2017-12-02 22:13:14 +0000

    Remove Font.style from createIconSetFromIcoMoon
    fbshipit-source-id: beee7d9

--- createIconSetFromIcoMoon.js
+++ createIconSetFromIcoMoon.js
@@ -2,5 +2,5 @@ import { Font } from 'expo';
 import createIconSetFromIcoMoon from './vendor/react-native-vector-icons/lib/create-icon-set-from-icomoon';
 
 export default function(config, expoFontName, expoAssetId) {
-  return createIconSetFromIcoMoon(config, Font.style(expoFontName).fontFamily, expoAssetId);
+  return createIconSetFromIcoMoon(config, expoFontName, expoAssetId);
 }
