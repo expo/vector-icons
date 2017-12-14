2d1dab13636ee0b4a13380fa940693fbc9adf391
Kévin Barralon <kevinbarralon@gmail.com>
2017-12-13 21:50:44 -0500

    remove Font.style from createIconSetFromFontello
    Not needed anymore. Since Expo 24, the function throws an error

--- createIconSetFromFontello.js
+++ createIconSetFromFontello.js
@@ -1,6 +1,5 @@
-import { Font } from 'expo';
 import createIconSetFromFontello from './vendor/react-native-vector-icons/lib/create-icon-set-from-fontello';
 
 export default function(config, expoFontName, expoAssetId) {
-  return createIconSetFromFontello(config, Font.style(expoFontName).fontFamily, expoAssetId);
+  return createIconSetFromFontello(config, expoFontName, expoAssetId);
 }
