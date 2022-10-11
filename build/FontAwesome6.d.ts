5af364ed94c8e0af7e50baaebb3b89f3509a6415
Gabriel Uhlíř <g.uhlir@gmail.com>
2022-10-11 12:49:38 +0200

    Support for FontAwesome6

---
+++ build/FontAwesome6.d.ts
@@ -0,0 +1,8 @@
+export declare const FA6Style: {
+    regular: string;
+    light: string;
+    solid: string;
+    brand: string;
+};
+declare const iconSet: any;
+export default iconSet;
---
+++ src/FontAwesome6.ts
@@ -0,0 +1,21 @@
+import { createFA6iconSet } from './createIconSetFromFontAwesome6';
+
+import glyphMap from './vendor/react-native-vector-icons/glyphmaps/FontAwesome6Free.json';
+import metadata from './vendor/react-native-vector-icons/glyphmaps/FontAwesome6Free_meta.json';
+const fontMap = {
+  Regular: require('./vendor/react-native-vector-icons/Fonts/FontAwesome6_Regular.ttf'),
+  Light: require('./vendor/react-native-vector-icons/Fonts/FontAwesome6_Regular.ttf'),
+  Solid: require('./vendor/react-native-vector-icons/Fonts/FontAwesome6_Solid.ttf'),
+  Brand: require('./vendor/react-native-vector-icons/Fonts/FontAwesome6_Brands.ttf'),
+};
+
+export const FA6Style = {
+  regular: 'regular',
+  light: 'light',
+  solid: 'solid',
+  brand: 'brand',
+};
+
+const iconSet = createFA6iconSet(glyphMap, metadata, fontMap, false);
+
+export default iconSet;
